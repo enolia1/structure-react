6abe116806d9c7e0fb0328aa39229159cb1382ea
Enolia Ritabile Monareng <enothabi@gmail.com>
2023-09-27 18:50:05 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import weather from "./weather";
+import Weather from "./Weather";
 import "./App.css";
 
 export default function App() {
@@ -7,7 +7,7 @@ export default function App() {
     <div className="App">
       <div className="container">
         <h1>weather App</h1>
-        <weather />
+        <Weather />
         <footer>
           This project was coded by{" "}
           <a
