9413bebdcaf2aa491c2b7e4d1cf6917d1414c00f
Enolia Ritabile Monareng <enothabi@gmail.com>
2023-09-27 19:57:14 +0200

    updated js ans css

--- src/weather.js
+++ src/weather.js
@@ -1,5 +1,43 @@
 import React from "react";
-
-export default function weather() {
-  return <div>Hello from weather</div>;
+import "./Weather.css";
+export default function Weather() {
+  return (
+    <div className="Weather">
+      <form>
+        <div className="row">
+          <div className="col-9">
+            <input
+              type="search"
+              placeholder="Enter a city..."
+              className="form-control"
+            />
+          </div>
+          <div className="col-3">
+            <input type="submit" value="Search" className="btn btn-primary" />
+          </div>
+        </div>
+      </form>
+      <h1>Bushbuckridge</h1>
+      <ul>
+        <li>wednessday 07:00</li>
+        <li>Mostly Cloudy</li>
+      </ul>
+      <div className="row">
+        <div className="col-6">
+          <img
+            src="https://ssl.gstatic.com/onebox/weather/64/partly_cloudy.png"
+            alt="Mostly Cloudy"
+          />
+          6°C
+        </div>
+        <div className="col-6">
+          <ul>
+            <li>Precipitation: 100%</li>
+            <li>Humidity: 98%</li>
+            <li>Wind: 8 mph</li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  );
 }
