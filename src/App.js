2452450768ee025c7e475fd8b4966783a5e2b715
Enolia Ritabile Monareng <enothabi@gmail.com>
2023-09-27 10:02:26 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -5,7 +5,13 @@ import "./App.css";
 export default function App() {
   return (
     <div className="App">
-      <h1>weather App</h1>
+      weather App
+      <footer>
+        This project was coded by Enolia Ritabile Monareng and is
+        <a href="https://github.com/enolia1/structure-react" target="_blank">
+          open sourced on GitHub
+        </a>
+      </footer>
     </div>
   );
 }
