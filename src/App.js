dca9bd4ef79966754cfed8e997b49d648975b690
Enolia Ritabile Monareng <enothabi@gmail.com>
2023-09-27 11:07:02 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -5,20 +5,22 @@ import "./App.css";
 export default function App() {
   return (
     <div className="App">
-      <h1>weather App</h1>
-      <footer>
-        This project was coded by{" "}
-        <a
-          href="https://www.shecodes.io/graduates/86838-enolia-ritabile-monareng"
-          target="_blank"
-        >
-          Enolia Ritabile Monareng
-        </a>{" "}
-        and is{" "}
-        <a href="https://github.com/enolia1/structure-react" target="_blank">
-          open sourced on GitHub
-        </a>
-      </footer>
+      <div className="container">
+        <h1>weather App</h1>
+        <footer>
+          This project was coded by{" "}
+          <a
+            href="https://www.shecodes.io/graduates/86838-enolia-ritabile-monareng"
+            target="_blank"
+          >
+            Enolia Ritabile Monareng
+          </a>{" "}
+          and is{" "}
+          <a href="https://github.com/enolia1/structure-react" target="_blank">
+            open sourced on GitHub
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
